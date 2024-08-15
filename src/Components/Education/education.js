b9bb35580f470ae411141842ea3bdf255af2d550
AdnanShaikh05 <159331841+AdnanShaikh05@users.noreply.github.com>
2024-08-15 16:55:49 +0530

    Update education.js

--- src/Components/Education/education.js
+++ src/Components/Education/education.js
@@ -6,7 +6,7 @@ function Education() {
     <>
     <Container className="my-5" style={{textAlign:"center"}}>
       <h1><i class="fa-solid fa-graduation-cap me-2"></i>Education</h1><hr/>
-      <Table striped bordered hover className="mt-4 ms-1 me-1">
+      <Table striped bordered hover className="mt-4">
         <thead >
           <tr >
               <th style={{backgroundColor:"rgb(176, 31, 176)", color:"white"}}>Passing Year</th>
